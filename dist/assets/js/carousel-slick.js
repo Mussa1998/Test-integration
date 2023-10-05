8515eb16d9ecc81a10adb5eff9b1dbe471ecc9cb
mussa1998 <mussamuhamed685@gmail.com>
2023-10-06 00:32:02 +0200

    last modif

--- dist/assets/js/carousel-slick.js
+++ dist/assets/js/carousel-slick.js
@@ -1,8 +1,48 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('.bloc-realisation').slick({
-        dots: true,
-        vertical: false,
+        infinite: true,
+        //vertical: false,
         slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
+    });
+});*/
+
+
+/*$('.bloc-realisation').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });*/
+
+/*$(document).ready(function(){
+    $('.bloc-realisation').slick({
+        infinite: true,
+        //vertical: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true
+    });
+});*/
+
+$(function(){
+    $('.bloc-for-slick').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        arrows: false,
+        slidesToScroll: 1
+    });
+});
+
+$(function(){
+    $('.slick-c').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        arrows: false,
         slidesToScroll: 3
     });
 });
+    
