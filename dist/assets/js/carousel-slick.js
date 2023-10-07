45d099ca8cb913dfe5ebd1222f68ef50724e7172
mussa1998 <mussamuhamed685@gmail.com>
2023-10-07 02:01:40 +0200

    nouvelles pages

--- dist/assets/js/carousel-slick.js
+++ dist/assets/js/carousel-slick.js
@@ -9,40 +9,63 @@
 });*/
 
 
-/*$('.bloc-realisation').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });*/
-
-/*$(document).ready(function(){
-    $('.bloc-realisation').slick({
+$(function(){
+    
+    $('.test-slick').slick({
         infinite: true,
-        //vertical: false,
+        dots: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        dots: true
+        responsive: [
+            {
+                breakpoint : 780,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            
+        ]
     });
-});*/
 
-$(function(){
-    $('.bloc-for-slick').slick({
+    
+    /*$('.test-slick').slick({
         dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        arrows: false,
-        slidesToScroll: 1
-    });
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });*/
 });
 
-$(function(){
-    $('.slick-c').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        arrows: false,
-        slidesToScroll: 3
-    });
-});
+
+
     
